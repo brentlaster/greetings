7ad0c72d414ac98a8ba45cb64439c5c83be11072
Brent Laster <Brent.Laster@sas.com>
2012-10-11 21:22:30 -0400

    initial commit on master

---
+++ helloWorkshop.java
@@ -0,0 +1,15 @@
+/* Hello Workshop Java Program */
+/* 9-12-12 */
+class helloWorkshop  {
+    public static void main(String[] args) {
+        System.out.println("Hello Workshop!");
+        System.out.println("This is an example file");
+        System.out.println("for use in workshop exercises.");
+        System.out.println("This is on the master branch");
+        System.out.println("This file is written in Java.");
+        System.out.println("Isn't this exciting?");
+        System.out.println("No?");
+        System.out.println("Okay then.");
+        System.out.println("Goodbye Workshop!");
+    }
+}
