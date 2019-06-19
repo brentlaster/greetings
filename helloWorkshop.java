19084ade9fdc22c907e3c26bfca028c80bc887cb
Petra Sargent <psargent@cloudbees.com>
2019-06-19 15:14:17 -0400

    THis is  test

--- helloWorkshop.java
+++ helloWorkshop.java
@@ -2,7 +2,7 @@
 /* 9-12-12 */
 class helloWorkshop  {
     public static void main(String[] args) {
-        System.out.println("Greetings Workshop!");
+        System.out.println("Greetings Everyone!");
         System.out.println("This is an example Java file");
         System.out.println("for use in workshop exercises.");
         System.out.println("This is on the master branch");
